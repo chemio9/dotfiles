0ce8751f21d0b8b518e1ce57ce9264d7bd0dd789
chenrry666 <chengruichen3@gmail.com>
2024-01-25 17:08:26 +0800

    feat(yazi;proxy): `HOST=127.0.0.1 proxy_on` usage; `ya` to change directory

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,8 +1,20 @@
-proxy_on(){ export HTTP_PROXY=http://127.0.0.1:7890 HTTPS_PROXY=127.0.0.1:7890 ALL_PROXY=http://127.0.0.1:7890 }
+proxy_on(){
+  [[ -n "$HOST" ]] && local HOST=127.0.0.1
+  export HTTP_PROXY=http://${HOST}:7890 HTTPS_PROXY=${HOST}:7890 ALL_PROXY=http://${HOST}:7890
+}
 proxy_off(){ unset HTTP_PROXY HTTPS_PROXY ALL_PROXY }
 
 alias md='mkdir -pv'
 alias mnt="rclone mount one:/ ${HOME}/one/ --vfs-cache-mode full &disown"
+
+function ya() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if local cwd="$(cat -- "$tmp")" &&  [[ -n "$cwd" ]]  && [[ "$cwd" != "$PWD" ]]; then
+		cd -- "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
 
 screenshot() {
 	sleep 1;
