e0163cd6b2360b1ce0b41c0268d1c46e66918646
Clarence <chengruichen3@gmail.com>
2026-02-14 22:57:24 +0800

    feat(zsh): aliases

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,11 +1,26 @@
 proxy_on(){
-  [[ -z "$1" ]] && local HOST=127.0.0.1 || HOST=$1
-  export HTTP_PROXY=http://${HOST}:7890 HTTPS_PROXY=http://${HOST}:7890 ALL_PROXY=http://${HOST}:7890
+  [[ -z "$HOST" ]] || local HOST=192.168.101.28
+  [[ -z "$PORT" ]] || local HOST=7890
+  export HTTP_PROXY=http://${HOST}:$PORT HTTPS_PROXY=http://${HOST}:$PORT ALL_PROXY=http://${HOST}:$PORT
+  export http_proxy=http://${HOST}:$PORT https_proxy=http://${HOST}:$PORT all_proxy=http://${HOST}:$PORT
 }
-proxy_off(){ unset HTTP_PROXY HTTPS_PROXY ALL_PROXY }
+proxy_off(){
+	unset HTTP_PROXY HTTPS_PROXY ALL_PROXY
+	unset http_proxy https_proxy all_proxy
+}
 
-alias md='mkdir -pv'
 alias mnt="rclone mount one:/ ${HOME}/one/ --vfs-cache-mode full &disown"
+alias :q="exit"
+alias :w="sync"
+alias :x="sync && exit"
+alias :wq="sync && exit"
+
+alias start="sudo systemctl start"
+alias stop="sudo systemctl stop"
+alias restart="sudo systemctl restart"
+
+alias .="source"
+alias cp="cp -i --reflink=auto --sparse=auto"
 
 # Change Yazi's CWD to PWD on subshell exit
 if [[ -n "$YAZI_ID" ]]; then
